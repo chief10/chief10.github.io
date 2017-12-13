eea36331fd3c3d67257b77183b055e96d611eafd
chief10 <mwvanmeurs@gmail.com>
2017-12-12 17:36:26 -0800

    Continue work on baby name graph

---
+++ _pages/popular-baby-names.md
@@ -0,0 +1,8 @@
+---
+layout: graph
+title: Popular Baby Names
+permalink: /popular-baby-names/
+custom_js: PopularBabyNames
+---
+
+Popular baby names!
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+
+const fs = require('fs');
+const webpack = require('webpack');
+
+
+module.exports = function () {
+  const config = {
+    entry: {},
+    output: {
+      filename: '[name].js',
+      path: __dirname + '/assets/js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.csv$/,
+          loader: 'csv-loader',
+          options: {
+            dynamicTyping: true,
+            header: false,
+            skipEmptyLines: true
+          }
+        }
+      ]
+    }
+  };
+
+  let graphNames = fs.readdirSync('./graphs');
+
+  graphNames.forEach((graph) => {
+    config.entry[graph] = `./graphs/${graph}/${graph}.js`
+  });
+
+
+  return config;
+}
